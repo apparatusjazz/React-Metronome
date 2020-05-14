45c3ea7a9b20df4ccdd0081dbc26a0d68171c65e
apparatusjazz <maui.arcuri@gmail.com>
2020-05-14 00:03:49 -0400

    Added function to pass accent change up to metronome

--- src/components/beat.js
+++ src/components/beat.js
@@ -1,11 +1,16 @@
 import React, { Component } from 'react'
 
 class Beat extends Component {
+    handleChange(evt, val) {
+        this.props.handleAccentChange(val, evt.target.value);
+    }
     render() {
         const beatStyle = { height: 150, width: 100, backgroundColor: "grey" }
         return (
             <div style={beatStyle}>
-                Beat
+                <form>
+                    <input type="text" defaultValue={this.props.accent.toString()} onChange={(e) => { this.handleChange(e, this.props.beatNo) }}></input>
+                </form>
             </div>
         )
     }

7cedec069d0893a50c2481c5f69a847a7376502c
apparatusjazz <maui.arcuri@gmail.com>
2020-05-15 03:19:00 -0400

    Fixed errors and added function to tap

--- src/components/tap.js
+++ src/components/tap.js
@@ -1,31 +1,32 @@
 import React, { Component } from 'react'
 
 class Tap extends Component {
-
     handleClick() {
-        if (typeof this.interval === undefined) {
-            this.interval = [];
-            this.time = new Date();
-        }
-        let newInterval = this.time.getTime() - this.time;
+        this.time = new Date();
+        let newInterval = this.time.getTime() - this.old;
+        this.old = this.time;
 
-        if (newInterval < 70000) {      //Don't do anything if interval is too big
+        if (newInterval < 1000) {      //Don't do anything if interval is too big
             this.interval.push(newInterval);
             this.time = this.time.getTime();
-            const avg;
+            let avg;
             // Get average
             if (this.interval.length >= 2) {
-                avg = this.interval.reduce((a, b) => {
-                    return (a + b) / this.interval.length;
-                }, 0);
-                this.props.bpmTap(avg);
+                let sum = this.interval.reduce((a, b) => a + b, 0)
+                avg = sum / this.interval.length;
+                this.props.bpmTap(Math.floor(60000 / avg));
             }
-        }
+        } else this.interval = [];
+    }
+    componentDidMount() {
+        this.interval = [];
+        this.time = new Date();
+        this.old = 0;
     }
     render() {
         return (
             <div>
-                <button onClick={this.handleClick}>Tap</button>
+                <button onClick={() => this.handleClick()}>Tap</button>
             </div>
         )
     }

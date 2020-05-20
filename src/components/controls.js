0ae0b645d0eedc2e3e428d79c5578e5e0daa9ef5
apparatusjazz <maui.arcuri@gmail.com>
2020-05-20 00:32:29 -0400

    Added time signature and play button

--- src/components/controls.js
+++ src/components/controls.js
@@ -1,10 +1,20 @@
 import React, { Component } from 'react'
+import TimeSignature from './time-signature';
 
+
+// Play button and bpm controls - slider, input
 class Controls extends Component {
+    handleClick() {
+        this.props.togglePlaying();
+    }
     render() {
         return (
             <div>
-
+                <button onClick={(e) => this.handleClick(e)}>Play</button>
+                <TimeSignature
+                    changeBeatNo={this.props.changeBeatNo}
+                    changeBeatLength={this.props.changeBeatLength}
+                />
             </div>
         )
     }

ecd8e270b24f5aad5223c1cf34d018083af91727
apparatusjazz <maui.arcuri@gmail.com>
2020-05-21 00:41:41 -0400

    Added buttons to change tempo incrementally

--- src/components/tempo.js
+++ src/components/tempo.js
@@ -4,10 +4,15 @@ class Tempo extends Component {
     handleChange(evt) {
         this.props.changeTempo(parseInt(evt.target.value));
     }
+    tempoChange(val) {
+        this.props.tempoChange(val);
+    }
     render() {
         return (
             <div>
+                <button className="bpmDown" onClick={() => this.tempoChange(0)}>--</button>
                 <input value={this.props.tempo} onChange={e => this.handleChange(e)}></input>
+                <button className="bpmUp" onClick={() => this.tempoChange(1)}>--</button>
             </div>
         )
     }

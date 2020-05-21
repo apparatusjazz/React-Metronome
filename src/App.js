ec6aad7b884bc0730f3d2c6f53d434cae04c7942
apparatusjazz <maui.arcuri@gmail.com>
2020-05-21 00:45:30 -0400

    Removed import

--- src/App.js
+++ src/App.js
@@ -1,7 +1,6 @@
 import React, { Component } from 'react';
 import './App.css';
 import Metronome from './components/metronome';
-import Beat from './components/beat';
 
 class App extends Component {
   render() {

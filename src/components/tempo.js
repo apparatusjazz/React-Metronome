376536d23e681c5bec9efac7583a406d96a1e475
apparatusjazz <maui.arcuri@gmail.com>
2020-05-22 01:56:51 -0400

    Added select all text onFocus

--- src/components/tempo.js
+++ src/components/tempo.js
@@ -1,17 +1,26 @@
 import React, { Component } from 'react'
 
 class Tempo extends Component {
+
     handleChange(evt) {
-        this.props.changeTempo(parseInt(evt.target.value));
+        let tempo = parseInt(evt.target.value);
+        if (isNaN(tempo)) tempo = this.props.tempo;
+        else this.props.changeTempo(tempo);
     }
     tempoChange(val) {
         this.props.tempoChange(val);
     }
+    handleFocus = (evt) => evt.target.select();
+
     render() {
         return (
             <div>
                 <button className="bpmDown" onClick={() => this.tempoChange(0)}>--</button>
-                <input value={this.props.tempo} onChange={e => this.handleChange(e)}></input>
+                <input
+                    value={this.props.tempo}
+                    onFocus={this.handleFocus}
+                    onChange={e => { this.handleChange(e) }}
+                ></input>
                 <button className="bpmUp" onClick={() => this.tempoChange(1)}>--</button>
             </div>
         )

09148e86849a5ca01a0fe3211f461b2b19cf4e0c
apparatusjazz <maui.arcuri@gmail.com>
2020-05-23 14:49:14 -0400

    Play button can be toggled with space

--- src/components/play.js
+++ src/components/play.js
@@ -2,13 +2,22 @@ import React, { Component } from 'react'
 
 class Play extends Component {
 
-    handleClcik(e) {
+    handleClick(e) {
         this.props.togglePlaying(e);
     }
+    handleKeyPress(e) {
+        if (e.key === "Space") {
+            this.props.togglePlaying(e);
+        }
+    }
+
     render() {
         return (
             <div>
-                <button onClick={(e) => this.handleClcik(e)}>Play</button>
+                <button
+                    onClick={(e) => this.handleClick(e)}
+                    onKeyDown={e => this.handleKeyPress(e)}
+                >Play</button>
             </div>
         )
     }
